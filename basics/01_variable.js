2a624ded61496af57e868f1a0d7b7acba6f4094c
Vishal Ingle <164838971+Inglevishal1999@users.noreply.github.com>
2025-01-09 16:16:35 +0000

    variable config done

---
+++ basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 14453;
+let accountEmail = 'Himesh@gmail.com';
+var accountPassword = '12345';
+accountCity = "Jaipue";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
